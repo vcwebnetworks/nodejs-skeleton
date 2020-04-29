a9ca8c30fcc3eee462a8556da60abe70aa773f2d
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-04-28 21:32:11 -0300

    refactor: melhorias no tratamento de erro

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -1,9 +1,7 @@
-export default class AppError {
-  public name: string;
-
+export default class AppError extends Error {
   constructor(public message: string, public statusCode = 400) {
+    super(message);
     this.name = 'AppError';
-    this.message = message;
     this.statusCode = statusCode;
   }
 }
--- src/middlewares/ErrorHandlerMiddleware.ts
+++ src/middlewares/ErrorHandlerMiddleware.ts
@@ -1,15 +1,22 @@
 import { NextFunction, Response, Request } from 'express';
-import AppError from '../errors/AppError';
+
+interface IError extends Error {
+  statusCode?: number;
+}
 
 export default function ErrorHandlerMiddleware(
-  error: Error,
+  error: IError,
   request: Request,
   response: Response,
   _: NextFunction,
 ): Response {
+  let statusCode = 500;
   const { name } = error;
   const message = error.message || 'Internal server error';
-  const statusCode = error instanceof AppError ? error.statusCode : 500;
+
+  if ('statusCode' in error) {
+    statusCode = error.statusCode as number;
+  }
 
   return response.status(statusCode).json({
     error: { name, statusCode, message },
