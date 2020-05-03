22ae49c41716b77e6b4329a3dfb47d622b55ed1a
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-05-02 23:57:24 -0300

    chore: remove '!'

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import app from './app';
 
 app.listen(process.env.PORT || 3333, () => {
   // eslint-disable-next-line no-console
-  console.log('🚀 Server started on port http://localhost:3333!');
+  console.log('🚀 Server started on port http://localhost:3333');
 });
