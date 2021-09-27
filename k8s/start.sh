ce687fc672f696fcc23e93f0cb344f6d1a5c4bae
Vagner Cardoso <vagnercardosoweb@gmail.com>
2021-09-26 22:02:18 -0300

    feat: k8s pvc postgres e improvements
    Signed-off-by: Vagner Cardoso <vagnercardosoweb@gmail.com>

--- k8s/start.sh
+++ k8s/start.sh
@@ -6,9 +6,7 @@ kubectl apply -f service.yaml
 kubectl apply -f environments.yaml
 kubectl apply -f redis.yaml
 kubectl apply -f postgres.yaml
-kubectl apply -f pvc-backend.yaml
+kubectl apply -f pvc-storage.yaml
 kubectl apply -f deployment.yaml
-
-# uncomment if using on manageable servers
 #kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.0/deploy/static/provider/cloud/deploy.yaml
 #kubectl apply -f ingress.yaml
