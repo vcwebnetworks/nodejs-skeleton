b2e43780eda7a58542bc70cc1d47dd76aa117943
Vagner Cardoso <vagnercardosoweb@gmail.com>
2022-01-10 00:53:10 -0300

    feat: add header in cors
    Signed-off-by: Vagner Cardoso <vagnercardosoweb@gmail.com>

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -7,11 +7,22 @@ export const corsMiddleware = (
 ) => {
   const allowOrigin = '*';
   const allowMethods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'];
-  const allowHeaders = request.headers['access-control-request-headers'];
+  const allowHeaders = [
+    'Accept',
+    'Origin',
+    'Content-Type',
+    'Authorization',
+    'Cache-Control',
+    'X-Requested-With',
+    'X-HTTP-Method-Override',
+    'X-Refresh-Token',
+    'X-Aws-IdToken',
+    'X-Test',
+  ];
 
   response.header('Access-Control-Allow-Origin', allowOrigin);
   response.header('Access-Control-Allow-Methods', allowMethods.join(','));
-  response.header('Access-Control-Allow-Headers', allowHeaders);
+  response.header('Access-Control-Allow-Headers', allowHeaders.join(','));
   response.header('Access-Control-Allow-Credentials', 'true');
 
   if (request.method.toUpperCase() === 'OPTIONS') {
